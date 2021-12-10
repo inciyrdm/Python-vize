16d7c1a4e61fcf052d3e0d2b0b4e0e1a644c52ea
inciyrdm <95236590+inciyrdm@users.noreply.github.com>
2021-12-11 00:15:44 +0300

    Add files via upload

---
+++ pythoninci/denemendeneme.py
@@ -0,0 +1,40 @@
+
+
+        
+
+
+
+class yemek():
+ 
+   
+    
+
+    malzemeler = input ("Malzemeler=")
+
+    tarif = input ("Tarifi=")
+    
+
+    print ("",malzemeler + tarif  )
+
+       
+
+
+
+
+f = open("yemektarifi.txt", "w")
+
+f.close()
+
+
+
+    
+
+
+
+import os.path
+print(os.getcwd())
+
+
+    
+
+    
---
+++ pythonöinci/python ödev.py
@@ -0,0 +1,40 @@
+file = open("yemekuyg.txt", "w")
+file.write("Yemek Tarifleri")
+file.close()
+
+import os.path
+print(os.getcwd())
+
+
+file = open("yemekuyg.txt", "a")
+file.write("\n yemek tarifi: Pamuk Poğaça Tarifi İçin Malzemeler \n 1 su bardağı ılık su \n 2 su bardağı ılık süt \n 1 su bardağı sıvı yağ1 adet yaş maya \n 2 yemek kaşığı şeker1 yemek kaşığı tuz \n 2 adet yumurta sarısı üzerine beyazı hamura \n Aldığı kadar un yaklaşık 6 su bardağı yinede kontrollü koyunİçi için; \n 1 kase beyaz peynir         ")
+file.close()
+
+
+
+file = open("yemekuyg2.txt", "w")
+file.write("Yemek Tarifleri")
+file.close()
+
+import os.path
+print(os.getcwd())
+
+
+file = open("yemekuyg2.txt", "a")
+file.write("\n yemek tarifi:    ")
+file.close()
+
+
+
+
+file = open("yemekuyg3.txt", "w")
+file.write("Yemek Tarifleri")
+file.close()
+
+import os.path
+print(os.getcwd())
+
+
+file = open("yemekuyg3.txt", "a")
+file.write("\n yemek tarifi:    ")
+file.close()
